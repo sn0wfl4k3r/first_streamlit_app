e49fef4fd7ad6c986f827978950806287170d24c
sn0wfl4k3r <113601079+sn0wfl4k3r@users.noreply.github.com>
2022-09-19 17:28:01 +0200

    Update streamlit_app.py
    nmew field

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,5 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
                                   
+fruit_proposal = streamlit.text_input('What fruit would You like to add?', 'jackfruit')
+streamlit.write('Thanks for adding', fruit_proposal)
